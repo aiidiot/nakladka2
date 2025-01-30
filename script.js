615cd6f97ae8e4e17fe67937b89c7b53139be77e
aiidiot <150623748+aiidiot@users.noreply.github.com>
2025-01-31 00:01:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ document.addEventListener('DOMContentLoaded', () => {
             shadow.classList.remove('sklejka');
             shadow.style.width = '10px';
             shadow.style.height = '120%';
-            shadow.style.left = '50%'; // Ustawiamy na środku
+            shadow.style.left = '40%'; // Ustawiamy na środku
             shadow.style.transform = 'translateX(-50%) rotate(7deg)'; // Centrujemy i obracamy
             shadow.style.top = '-10%';
             shadow.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
